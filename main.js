02e4fbf63871a7e6ad33561b55b6703bfe2de256
MattM2004 <97617857+MattM2004@users.noreply.github.com>
2022-01-20 10:16:53 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+var objPeople = [
+    {
+        email: "sam@gmail.com",
+        password: "codify"
+    },
+    {
+        email:"matt@gmail.com",
+        password:"academy"
+    },
+    {
+        email:"chris@gmail.com",
+        password:"forever"
+    }
+]
+
+function getInfo(){
+    var email = document.getElementById("email").value
+    var password = document.getElementById("password").value
+
+    for(i = 0; i < objPeople.length; i++){
+        if(email == objPeople[i].email && password == objPeople[i].password){
+            window.location.href = "ticket.html"
+            return
+        } 
+    }
+}
