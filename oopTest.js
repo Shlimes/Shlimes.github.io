c32583db27f22a19ae527d9c4dcdca06c1156ede
OwenC16 <83368252+OwenC16@users.noreply.github.com>
2022-01-21 15:27:24 -0500

    Create oopTest.js

---
+++ oopTest.js
@@ -0,0 +1,32 @@
+//test in repl
+function CreateTicket() {
+
+function TicketObject(email, grade, type, comments, date, identifier) {
+  this.email = email;
+  this.grade = grade;
+  this.type = type;
+  this.comments = comments;
+  this.date = date;
+  this.identifier = identifier;
+}
+TicketObject.prototype.setDate = function(date) {
+  var todayDate = new Date().toISOString().slice(0, 10);
+  ticket.date = todayDate;
+  return(date);
+}
+TicketObject.prototype.setIdentifier = function(identifier) {
+  var randomNum = Math.floor((Math.random() * 999999) + 100000);
+  ticket.identifier = randomNum;
+  return(identifier);
+}
+
+var txtEmail = document.getElementById("email");
+var txtGrade = document.getElementsByName("grade");
+var txtType = document.getElementsByName("type");
+var txtComments = document.getElementsByName("comments");
+
+let ticket = new TicketObject(txtEmail, txtGrade, txtType, txtComments, "", "")
+ticket.setDate();
+ticket.setIdentifier();
+console.log(ticket);
+}
