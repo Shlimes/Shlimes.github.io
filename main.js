501a67b08603b64fdd1a254f33d5df6bb11af01a
jordanbelanger <83415370+jordanbelanger@users.noreply.github.com>
2022-01-24 11:41:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,7 @@
-var objPeople = [
-    {
-        email: "sam@gmail.com",
-        password: "codify"
-    },
-    {
-        email:"matt@gmail.com",
-        password:"academy"
-    },
-    {
-        email:"chris@gmail.com",
-        password:"forever"
-    }
-]
-
-function getInfo(){
-    var email = document.getElementById("email").value
-    var password = document.getElementById("password").value
-
-    for(i = 0; i < objPeople.length; i++){
-        if(email == objPeople[i].email && password == objPeople[i].password){
-            window.location.href = "ticket.html"
-            return
-        } 
-    }
-}
-
 function passwordAdmin(){
     var password = document.getElementById("password").value;
     var password1 = "123"
     if (password == password1) {
-        window.location.href = "http://www.w3schools.com";
+        window.location.href = "admin.html";
     }
 }
